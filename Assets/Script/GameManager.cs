33be9ab5ace64f27564f12b7c002634f34820e7a
h0ldtighttt <h0ldtighttt@gmail.com>
2025-03-20 21:12:01 +0900

    save

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private static GameManager Instance;
+    public UserData userData;
+
+    public void SetUserData()
+    {
+        userData = new UserData("ø¿øÏ≈√", 100000, 50000);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // SetUserData();
     }
 
     // Update is called once per frame
--- Assets/Script/UserData.cs
+++ Assets/Script/UserData.cs
@@ -1,11 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
+using Unity.VisualScripting;
+using UnityEngine;
 public class UserData : MonoBehaviour
 {
     
-    public string UserName;
-    public int UserMoney;
-    public int Cash;
+    public string userName { get; set; } = "";
+    public int userCash { get; set; } = 0;
+    public int userBalance { get; set; } = 0;
+    
+    public UserData(string name, int cash, int balance)
+    {
+        userName = name;
+        userCash = balance;
+        userBalance = balance;
+    }
+
 }
