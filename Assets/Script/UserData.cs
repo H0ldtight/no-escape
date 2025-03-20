3436ac653a8d4204a482c979fcac90c695def946
h0ldtighttt <h0ldtighttt@gmail.com>
2025-03-20 17:17:43 +0900

    fix : canvas_Reference_Resolution

---
+++ Assets/Script/UserData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserData : MonoBehaviour
+{
+    
+    public string UserName;
+    public int UserMoney;
+    public int Cash;
+}
